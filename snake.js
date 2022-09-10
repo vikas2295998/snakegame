473eeff9c04c08a4c5f2b18b4945de6cfe861f4a
vikas kumar <vikassaraimeer1999@gmail.com>
2022-09-10 15:22:44 +0530

    added some css and music

--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
- let grid=document.getElementById("gamearea");
+let grid=document.getElementById("gamearea");
 
 //         var node = document.createElement("div");
 //         node.innerHTML="vikas";
@@ -7,10 +7,14 @@
 //          node.classList.add('sbody');
 //         grid.appendChild(node);
     
-
+const background= new Audio('background.mp3');
+const eat_music= new Audio('eat_food.mp3');
+const gameover_music= new Audio('gameover.mp3');
 
 var snake=[{x:10,y:12}];
 var food={x:14,y:6};
+    food.x=Math.round(2+16*Math.random());
+    food.y=Math.round(2+16*Math.random());
 var snakedir={x:0,y:0};
 var ptime=0;
 var speed=4;
@@ -25,13 +29,16 @@ function main(ctime){
     gameEngine();
 }
 function isColide(){
+    if(snake[0].x<0|| snake[0].x>=19|| snake[0].y<0|| snake[0].y>=19)
+    return true;
     for(let i=1;i<snake.length;i++){
-        if(snake[0].x==snake[i].x&&snake[0].y==snake[i].y)
+        if(snake[0].x==snake[i].x&&snake[0].y==snake[i].y ) 
         return true;
     }
     return false;
 }
 function gamover(){
+    speed=0;
     document.getElementById('gameover').style.display='block';
     setTimeout(() => {
         document.getElementById('gameover').style.display='none';
@@ -46,14 +53,17 @@ function gameEngine(){
     document.getElementById('highest').innerHTML=`highest score :${hs}`;
     localStorage.setItem('highScore',hs);
     if(isColide()){
+        gameover_music.play();
         score=0;
         snake=[{x:10,y:12}];
         food={x:14,y:6};
         ptime=0;
-        alert("game over");
+        background.pause();
+        gamover();
         
     }
     if(snake[0].x==food.x&&snake[0].y==food.y){
+        eat_music.play();
         score++;
         x1=snakedir.x+food.x;
         y1=snakedir.y+food.y;
@@ -69,7 +79,7 @@ function gameEngine(){
     }
     snake[0].x=(snake[0].x+snakedir.x)%20;
     snake[0].y=(snake[0].y+snakedir.y)%20;
-
+  
     var node1 = document.createElement("div");
     node1.style.gridRowStart=food.x;
     node1.style.gridColumnStart=food.y;
@@ -92,6 +102,8 @@ function gameEngine(){
 
 }
 window.addEventListener('keydown',e=>{
+    background.play();
+    speed=4;
     switch (e.code) {
         case 'ArrowUp':
             snakedir.x=-1;
